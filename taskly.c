8b30464e48cd4d98b6b472e2a853185a9c3127f9
Jarred Parr <jparr721@gmail.com>
2018-01-13 10:03:37 -0500

    Holy bugfixes, batman

--- taskly.c
+++ taskly.c
@@ -3,33 +3,38 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define VERSION_NUMBER "0.1.0"
 
+char* create_new_note(char* note_name) {
+	printf("Generating new list");
+	FILE *f_name = fopen(note_name, "a");
 
+	return "Note created successfully";
+}
 
-void handle_flags(int in_length, char* input[]) {
+int main(int argc, char* argv[]) {
 	int choice;
-	while((choice = getopt (argc, argv, "hnucd:")) != -1)
-		switch(input[i]) {
-			case '-h':
-				printf("Usage:");
+	while((choice = getopt (argc, argv, "hnucd:")) != -1) {
+		switch(choice) {
+			case 'h':
+				printf("Taskkly version: %s", VERSION_NUMBER);
+				printf("\nUsage:");
 				printf("taskly command [options] [list_name]");
 				printf("-h	Displays the help options");
 				printf("-n	Create a new task list");
 				printf("-u	Update existing list");
 				printf("-c	Complete a task");
+				printf("-l	List all existing task lists");
 				printf("-d	Delete an existing task or task list");
 			case 'n':
 				create_new_note(optarg);
 			case 'u':
-				update_task_list(optarg);
+//				update_task_list(optarg);
 			case 'c':
-				complete_task(optarg);
+//				complete_task(optarg);
 			case 'd':
-				delete_task
+				break;
+//				delete_task();
 		}
 	}
-}
-
-int main(int argc, char* argv[]) {
-	handle_flags(argc, argv[]);
 }
--- taskly.h
+++ taskly.h
@@ -1,5 +1,3 @@
 #pragma once
 
-#define VERSION_NUMBER = 0.1.0;
-
-void handle_flags(int in_length. char* input[]);
+char* create_new_note(char*);
